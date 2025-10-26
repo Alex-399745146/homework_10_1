c5e262ad767bc1e41141003b6cccd7e5e9568f28
Alexsandr Bachevskiy <bachevskiiaa@gmail.com>
2025-10-27 00:25:46 +0300

    changed the check in main

--- main.py
+++ main.py
@@ -4,11 +4,17 @@
 """
 
 from src.masks import get_mask_account, get_mask_card_number
+from src.widget import mask_account_card
 
 card_number = "7000792289606361"
 
 account_number = "73654108430135874305"
 
+open_string = "Visa Platinum 7000792289606361"
+open_string_1 = "Счет 73654108430135874305"
+
 
 print(get_mask_card_number(card_number))
 print(get_mask_account(account_number))
+print(mask_account_card(open_string))
+print(mask_account_card(open_string_1))
