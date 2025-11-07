0454957673935509c75c13146bdeb6a37321f97e
Alexsandr Bachevskiy <bachevskiiaa@gmail.com>
2025-11-07 23:07:46 +0300

    add def filter_by_state() inside finish

--- src/processing.py
+++ src/processing.py
@@ -4,5 +4,18 @@
 """
 
 
-def filter_by_state():
-    pass
+def filter_by_state(ls_positions: list, status: str = "EXECUTED") -> list:
+    """
+    Принимает список словарей и опционально значение для ключа
+    state по умолчанию 'EXECUTED'.
+    :return:
+    Возвращает новый список словарей, содержащий только те словари, у которых ключ
+    state соответствует указанному значению.
+    """
+    new_ls_position = []
+
+    for dict_position in ls_positions:
+        if dict_position["state"] == status:
+            new_ls_position.append(dict_position)
+
+    return new_ls_position
