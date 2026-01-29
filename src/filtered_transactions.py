f263f054dd4872b7c9df03d8a369d5b011a2dbb6
Alexsandr Bachevskiy <bachevskiiaa@gmail.com>
2026-01-29 21:21:14 +0300

    finish add two func in mod_filtred_transactions

--- src/filtered_transactions.py
+++ src/filtered_transactions.py
@@ -5,6 +5,8 @@
 
 import os
 import re
+from collections import defaultdict
+from typing import Any
 
 from dotenv import load_dotenv
 
@@ -12,11 +14,11 @@ from src.data_extractor import get_info_csv
 
 
 def process_bank_search(data: list[dict], search_text: str) -> list[dict]:
-    """Фильтруем трансакции по ключевому слову"""
+    """Фильтрация трансакции по ключевому слову"""
     pattern = re.compile(search_text)
     result = []
     for transaction in data:
-        if pattern.search(str(transaction["description"])):
+        if pattern.search(str(transaction)):
             result.append(transaction)
         else:
             continue
@@ -24,14 +26,33 @@ def process_bank_search(data: list[dict], search_text: str) -> list[dict]:
     return result
 
 
+def process_bank_operations(data: list[dict], categories: list) -> dict:
+    """Фильтрация трансакции по категориям с подсчётом операций в категориях"""
+    result: Any = defaultdict(int)
+    for transaction in data:
+        category = transaction.get("description")
+
+        if category in categories:
+            result[category] += 1
+
+    return dict(result)
+
+
 if __name__ == "__main__":
     load_dotenv()  # Загрузка переменных из .env-файла.
     file_csv_path = os.getenv("FILE_PATH_CSV", "default_log_file.csv")
-    search_text = "Перевод организации"
 
     data = get_info_csv(file_csv_path)
+    search_text = "EXECUTED"
+    categories = ["Перевод организации", "Перевод с карты на карту", "Перевод со счета на счет"]
 
-    filter_data = process_bank_search(data, search_text)
+    filter_data_1 = process_bank_search(data, search_text)
+    filter_data_2 = process_bank_operations(data, categories)
 
-    for item in filter_data:
-        print(item)
+    # Для 1ой функции
+    for transaction in filter_data_1:
+        print(transaction)
+
+    # Для 2ой функции
+    # for key, value in filter_data_2.items():
+    #     print(key, value)
